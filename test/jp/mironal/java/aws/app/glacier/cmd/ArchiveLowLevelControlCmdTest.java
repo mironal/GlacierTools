0b10fca43d9e37cb314d682e50740fa4e0d5af7e
mironal <miro.gunp@gmail.com>
2012-09-01 17:48:54 +0900

    テスト項目追加

--- test/jp/mironal/java/aws/app/glacier/cmd/ArchiveLowLevelControlCmdTest.java
+++ test/jp/mironal/java/aws/app/glacier/cmd/ArchiveLowLevelControlCmdTest.java
@@ -2,6 +2,7 @@
 package jp.mironal.java.aws.app.glacier.cmd;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -27,6 +28,18 @@ public class ArchiveLowLevelControlCmdTest {
     }
 
     @Test
+    public void test_inventoryRetrieval_InvalidParam() {
+        ArchiveLowLevelControlCmd cmd = new ArchiveLowLevelControlCmd(new String[] {
+                "inventory-retrieval", "--debug"
+        });
+
+        assertEquals(cmd.cmdKind, Kind.Inventory);
+        assertEquals(cmd.vaultname, null);
+
+        assertFalse(cmd.validateInventoryParam());
+    }
+
+    @Test
     public void test_inventoryRetrievalWithEndpoint() {
         ArchiveLowLevelControlCmd cmd = new ArchiveLowLevelControlCmd(new String[] {
                 "inventory-retrieval", "--vault", "hogehoge", "--endpoint", "us-east-1"
@@ -35,6 +48,7 @@ public class ArchiveLowLevelControlCmdTest {
         assertEquals(cmd.cmdKind, Kind.Inventory);
         assertEquals(cmd.vaultname, "hogehoge");
         assertEquals(cmd.region, Region.US_EAST_1);
+        assertTrue(cmd.validateInventoryParam());
     }
 
     @Test
@@ -46,6 +60,7 @@ public class ArchiveLowLevelControlCmdTest {
         assertEquals(cmd.cmdKind, Kind.Inventory);
         assertEquals(cmd.vaultname, "hogehoge");
         assertEquals(cmd.syncType, Sync.Sync);
+        assertTrue(cmd.validateInventoryParam());
 
         cmd = new ArchiveLowLevelControlCmd(new String[] {
                 "inventory-retrieval", "async", "--vault", "hogehoge",
@@ -53,18 +68,58 @@ public class ArchiveLowLevelControlCmdTest {
         assertEquals(cmd.cmdKind, Kind.Inventory);
         assertEquals(cmd.vaultname, "hogehoge");
         assertEquals(cmd.syncType, Sync.Async);
+        assertTrue(cmd.validateInventoryParam());
 
     }
 
     @Test
     public void test_archiveRetrieval() {
         ArchiveLowLevelControlCmd cmd = new ArchiveLowLevelControlCmd(new String[] {
-                "archive-retrieval", "--vault", "homuhomu", "--archive", "archiveId"
+                "archive-retrieval", "--vault", "homuhomu", "--archive", "archiveId", "--file",
+                "hoge.zip"
         });
 
         assertEquals(cmd.cmdKind, Kind.Archive);
         assertEquals(cmd.vaultname, "homuhomu");
         assertEquals(cmd.archiveId, "archiveId");
+        assertEquals(cmd.saveFile, "hoge.zip");
+        assertTrue(cmd.validateInventoryParam());
+    }
+
+    @Test
+    public void test_archiveRetrieval_InvalidParam() {
+        // vault無し
+        ArchiveLowLevelControlCmd cmd = new ArchiveLowLevelControlCmd(new String[] {
+                "archive-retrieval", "--archive", "archiveId", "--file", "hoge.zip"
+        });
+
+        assertEquals(cmd.cmdKind, Kind.Archive);
+        assertEquals(cmd.vaultname, null);
+        assertEquals(cmd.archiveId, "archiveId");
+        assertEquals(cmd.saveFile, "hoge.zip");
+        assertFalse(cmd.validateInventoryParam());
+
+        // archive無し
+        cmd = new ArchiveLowLevelControlCmd(new String[] {
+                "archive-retrieval", "--vault", "homuhomu", "--file", "hoge.zip"
+        });
+
+        assertEquals(cmd.cmdKind, Kind.Archive);
+        assertEquals(cmd.vaultname, "homuhomu");
+        assertEquals(cmd.archiveId, null);
+        assertEquals(cmd.saveFile, "hoge.zip");
+        assertFalse(cmd.validateInventoryParam());
+
+        // file無し
+        cmd = new ArchiveLowLevelControlCmd(new String[] {
+                "archive-retrieval", "--vault", "homuhomu", "--archive", "archiveId",
+        });
+
+        assertEquals(cmd.cmdKind, Kind.Archive);
+        assertEquals(cmd.vaultname, "homuhomu");
+        assertEquals(cmd.archiveId, "archiveId");
+        assertEquals(cmd.saveFile, null);
+        assertFalse(cmd.validateInventoryParam());
     }
 
     @Test
@@ -74,6 +129,17 @@ public class ArchiveLowLevelControlCmdTest {
         });
         assertEquals(cmd.cmdKind, Kind.List);
         assertEquals(cmd.vaultname, "aaaaaa");
+        assertTrue(cmd.validateInventoryParam());
+    }
+
+    @Test
+    public void test_list_InvalidParam() {
+        ArchiveLowLevelControlCmd cmd = new ArchiveLowLevelControlCmd(new String[] {
+            "list",
+        });
+        assertEquals(cmd.cmdKind, Kind.List);
+        assertEquals(cmd.vaultname, null);
+        assertFalse(cmd.validateInventoryParam());
     }
 
     @Test
@@ -84,6 +150,29 @@ public class ArchiveLowLevelControlCmdTest {
         assertEquals(cmd.cmdKind, Kind.Describe);
         assertEquals(cmd.vaultname, "bbbbb");
         assertEquals(cmd.jobId, "jobId");
+        assertTrue(cmd.validateInventoryParam());
+    }
+
+    @Test
+    public void test_desc_InvalidParam() {
+        // vault無し
+        ArchiveLowLevelControlCmd cmd = new ArchiveLowLevelControlCmd(new String[] {
+                "desc", "--job", "jobId"
+        });
+        assertEquals(cmd.cmdKind, Kind.Describe);
+        assertEquals(cmd.vaultname, null);
+        assertEquals(cmd.jobId, "jobId");
+        assertFalse(cmd.validateInventoryParam());
+
+        // job無し
+        cmd = new ArchiveLowLevelControlCmd(new String[] {
+                "desc", "--vault", "bbbbb",
+        });
+        assertEquals(cmd.cmdKind, Kind.Describe);
+        assertEquals(cmd.vaultname, "bbbbb");
+        assertEquals(cmd.jobId, null);
+        assertFalse(cmd.validateInventoryParam());
+
     }
 
     @Test
@@ -122,6 +211,7 @@ public class ArchiveLowLevelControlCmdTest {
         });
 
         assertEquals(cmd.region, Region.AP_NORTHEAST_1);
+        assertTrue(cmd.validateInventoryParam());
 
     }
 
@@ -132,6 +222,6 @@ public class ArchiveLowLevelControlCmdTest {
         });
         assertNull(cmd.region);
         assertEquals(cmd.cmdKind, Kind.Bad);
-
     }
+
 }

c0a064bab8595a1cf00512b98c6194fd5bcca657
mironal <miro.gunp@gmail.com>
2012-09-02 17:22:40 +0900

    Jobのリストア機能追加

--- src/jp/mironal/java/aws/app/glacier/cmd/RestoreJobController.java
+++ src/jp/mironal/java/aws/app/glacier/cmd/RestoreJobController.java
@@ -1,95 +1,142 @@
 
 package jp.mironal.java.aws.app.glacier.cmd;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
-public class RestoreJobController {
+public class RestoreJobController extends CmdUtils {
+
+    enum RestoreJobCmdKind {
+        Bad, Download, Check, Desc, Help
+    }
 
     @SuppressWarnings("serial")
-    class JobRestoreParamNotFoundException extends RuntimeException {
-        public JobRestoreParamNotFoundException(String msg) {
+    class JobRestoreException extends RuntimeException {
+        public JobRestoreException(String msg) {
             super(msg);
         }
     }
 
     String jobId;
     String vaultname;
+    RestoreJobCmdKind cmdKind;
 
-    public RestoreJobController(String[] args) {
-
+    public RestoreJobController(String[] args) throws IOException {
+        String restorePropFilename = null;
+        String propertiesName = null;
         for (int i = 0; i < args.length; i++) {
             String arg = args[i];
+            if (arg.equals("download")) {
+                setCmdKind(RestoreJobCmdKind.Download);
+            }
+            if (arg.equals("check")) {
+                setCmdKind(RestoreJobCmdKind.Check);
+            }
+            if (arg.equals("desc")) {
+                setCmdKind(RestoreJobCmdKind.Desc);
+            }
 
-            if (arg.equals("--vault")) {
+            if (arg.equals("-h") || arg.equals("--help") || arg.equals("help")) {
+                setCmdKind(RestoreJobCmdKind.Help);
+            }
+
+            if (arg.equals("--restore")) {
                 if ((i + 1) < args.length) {
                     i++;
-                    vaultname = args[i];
+                    restorePropFilename = args[i];
                 }
             }
 
-            if (arg.equals("--job")) {
-                if ((i + 1) < arg.length()) {
+            if (arg.equals("--properties")) {
+                if ((i + 1) < args.length) {
                     i++;
-                    jobId = args[i];
+                    propertiesName = args[i];
                 }
             }
+
+        }
+
+        // プロパティーファイルが無かったらcmdをBadにする.
+        setAwsCredentialsPropertiesFile(propertiesName);
+
+        restoreJobParam(restorePropFilename);
+    }
+
+    private void setCmdKind(RestoreJobCmdKind cmd) {
+        if (this.cmdKind == RestoreJobCmdKind.Bad) {
+            this.cmdKind = cmd;
+        } else {
+            /* オプション違反 */
+            this.cmdKind = RestoreJobCmdKind.Bad;
         }
     }
 
-    private void loadJobRestoreParam(String filename) throws FileNotFoundException, IOException {
+    private void restoreJobParam(String filename) throws IOException {
         if (filename == null) {
-            throw new NullPointerException("filename is null.");
+            throw new JobRestoreException("--restore option is not specified");
         }
-
+        File restoreFile = new File(filename);
+        if (!restoreFile.exists()) {
+            throw new JobRestoreException(restoreFile.getAbsolutePath() + " does not exist.");
+        }
         Properties properties = new Properties();
-        properties.load(new FileReader(filename));
-
-        if (properties.contains("JobId")) {
-            throw new JobRestoreParamNotFoundException("JobId not found.");
-        }
-        if (properties.contains("VaultName")) {
-            throw new JobRestoreParamNotFoundException("VaultName not found.");
+        try {
+            properties.load(new FileReader(filename));
+        } catch (FileNotFoundException ignore) {
+            // 無視するが念のためStackTraceは出す.
+            ignore.printStackTrace();
         }
 
-        jobId = properties.getProperty("JobId");
-        vaultname = properties.getProperty("VaultName");
+        if (!properties.contains("JobId")) {
+            throw new JobRestoreException("JobId not found.");
+        }
+        if (!properties.contains("VaultName")) {
+            throw new JobRestoreException("VaultName not found.");
+        }
+        if (!properties.contains("Region")) {
+            throw new JobRestoreException("Region not found.");
+        }
+
+        this.jobId = properties.getProperty("JobId");
+        this.vaultname = properties.getProperty("VaultName");
+        setRegion(properties.getProperty("Region"));
+    }
+
+    @Override
+    void onAwsCredentialsPropertiesFileNotFound(String filename, Throwable e) {
+
+    }
+
+    @Override
+    void onRegionNotFound(String endpointStr, Throwable e) {
+
+    }
+
+    @Override
+    void onExecCommand() throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    void onExecInvalidParam() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    boolean validateParam() {
+        // TODO Auto-generated method stub
+        return false;
     }
 
     /**
-     * * java -jar lowlevelControl.jar [restore] [jobType] {syncType}<br>
-     * [--restore restore_prop_filename]<br>
-     * <br>
-     * [jobType] = download | check<br>
-     * [syncType] = sync | async<br>
-     * [endpoint] = ushogehoge<br>
-     * <br>
-     * <br>
-     * restore_prop_filename(filename.properties)の中身は<br>
-     * JobId=jobID VaultName=vaultname<br>
-     * SnsSubscriptionArn=snsSubscriptionArn<br>
-     * SnsTopicArn=snsTopicArn<br>
-     * SqsQueueUrl=sqsQueueUrl<br>
-     * の用になっていてJavaのPropertiesで読める形にしておく<br>
-     * <br>
-     * こうすることでAsyncでコマンドを実行する際にその出力をhoge.propertiesにリダイレクトすることで、<br>
-     * Jobの復元を容易に行うことが出来る. <br>
-     * <br>
-     * * Restore Job<br>
-     * java -jar lowlevelControl.jar [restore] [jobType] {syncType}<br>
-     * [--job jobId]<br>
-     * [--vault vaultname]<br>
-     * [--snssub snsSubscriptionArn]<br>
-     * [--snstopic snsTopicArn]<br>
-     * [--sqsurl sqsQueueUrl]<br>
-     * <br>
-     * 
      * @param args
      */
     public static void main(String[] args) {
 
     }
-
 }
--- test/jp/mironal/java/aws/app/glacier/cmd/RestoreJobControllerTest.java
+++ test/jp/mironal/java/aws/app/glacier/cmd/RestoreJobControllerTest.java
@@ -3,12 +3,39 @@ package jp.mironal.java.aws.app.glacier.cmd;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class RestoreJobControllerTest {
 
+    private static final String RESTORE_FILENAME = "RestoreJobParam.properties";
+
+    @BeforeClass
+    public void setupRestoreFile() throws FileNotFoundException {
+        File file = new File(RESTORE_FILENAME);
+        PrintWriter pw = new PrintWriter(file);
+        pw.println("JobId=hogehoge");
+        pw.println("VaultName=piyopiyo");
+        pw.print("Region=us-west-2");
+        pw.close();
+    }
+
+    @AfterClass
+    public void deleteRestoreFile() {
+        File file = new File(RESTORE_FILENAME);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
     @Test
-    public void test() {
+    public void test_Download() {
+
     }
 
 }

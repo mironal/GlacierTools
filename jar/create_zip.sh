62a7e2a59b3429ac451c92ca94cca9e7d805f523
mironal <miro.gunp@gmail.com>
2012-09-09 16:34:50 +0900

    ダウンロード用ファイルにまとめる為のスクリプトを書いた

---
+++ jar/create_zip.sh
@@ -0,0 +1,2 @@
+zip GlacierTools.zip vault_controller.jar vault_controller.sh vault_controller.bat archive_controller.jar archive_controller.sh archive_controller.bat job_operator.jar restore_job.jar
+

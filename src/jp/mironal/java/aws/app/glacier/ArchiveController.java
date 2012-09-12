95903778289ae1f8e96cf78f1023b3e752b21992
mironal <miro.gunp@gmail.com>
2012-09-12 17:29:14 +0900

    コメント追加

--- src/jp/mironal/java/aws/app/glacier/ArchiveController.java
+++ src/jp/mironal/java/aws/app/glacier/ArchiveController.java
@@ -9,12 +9,28 @@ import com.amazonaws.services.glacier.model.DeleteArchiveRequest;
 import com.amazonaws.services.glacier.transfer.ArchiveTransferManager;
 import com.amazonaws.services.glacier.transfer.UploadResult;
 
+/**
+ * @author mironal
+ */
 public class ArchiveController extends GlacierTools {
 
+    /**
+     * デフォルトコンストラクタ<br>
+     * デフォルトのリージョンとカレントディレクトリにあるAwsCredentials.propertiesを使用してインスタンスを作成
+     * 
+     * @throws IOException
+     */
     public ArchiveController() throws IOException {
         super();
     }
 
+    /**
+     * 指定したリージョンとAwsCredentials.propertiesファイルでインスタンスを作成
+     * 
+     * @param endpoint リージョン
+     * @param awsProperties AwsCredentials.propertiesファイルのインスタンス
+     * @throws IOException
+     */
     public ArchiveController(Region endpoint, File awsProperties) throws IOException {
         super(endpoint, awsProperties);
     }
@@ -22,10 +38,11 @@ public class ArchiveController extends GlacierTools {
     /**
      * アーカイブのアップロード
      * 
-     * @param vaultName
-     * @param description
-     * @param file アップロードするファイル.
-     * @return
+     * @param vaultName The name of the vault to upload to.
+     * @param description The description of the new archive being uploaded.
+     * @param file The file to upload to Amazon Glacier.
+     * @return The result of the upload, including the archive ID needed to
+     *         access the upload later.
      * @throws FileNotFoundException
      */
     public UploadResult upload(String vaultName, String description, File file)
@@ -40,9 +57,9 @@ public class ArchiveController extends GlacierTools {
     /**
      * アーカイブのダウンロード
      * 
-     * @param vaultName
-     * @param archiveId
-     * @param file ダウンロードしたファイルの保存先.
+     * @param vaultName The name of the vault to download the archive from.
+     * @param archiveId The unique ID of the archive to download.
+     * @param file The file save the archive to.
      */
     public void download(String vaultName, String archiveId, File file) {
         ArchiveTransferManager atm = new ArchiveTransferManager(client, credentials);
@@ -51,6 +68,9 @@ public class ArchiveController extends GlacierTools {
 
     /**
      * アーカイブの削除
+     * 
+     * @param vaultName The name of the vault to delete the archive from.
+     * @param archiveId The unique ID of the archive to delete.
      */
     public void delete(String vaultName, String archiveId) {
         DeleteArchiveRequest dar = new DeleteArchiveRequest().withVaultName(vaultName)

d7a037c5a1c58cb2d0d98334464f7cbe8a2a6a5e
mironal <miro.gunp@gmail.com>
2012-09-18 07:09:50 +0900

    zipにバージョン名を付加するようにした

--- jar/create_zip.sh
+++ jar/create_zip.sh
@@ -1,2 +1,2 @@
-zip GlacierTools.zip vault_controller.jar vault_controller.sh vault_controller.bat archive_controller.jar archive_controller.sh archive_controller.bat job_operator.jar restore_job.jar
+zip GlacierTools-0.1.zip vault_controller.jar vault_controller.sh vault_controller.bat archive_controller.jar archive_controller.sh archive_controller.bat job_operator.jar restore_job.jar
 
